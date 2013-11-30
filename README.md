55dbc68e44956f6bedb16f1f1a09378be710e47d
Mark Risher <m@impr.co>
2013-11-29 16:17:01 -0800

    successfully reads XML for a specified date

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ToDo:
 
-* Sample query: http://api.amp.active.com/camping/campsites?contractCode=CA&parkId=120063&arvdate=01%2F14%2F2014&lengthOfStay=1&siteType=10001&api_key=3vtcjh2jfsnh78z7bjbfkdmj
+* Sample query: 
+
+http://api.amp.active.com/camping/campsites?contractCode=CA&parkId=120063&arvdate=01%2F14%2F2014&lengthOfStay=1&siteType=10001&api_key=3vtcjh2jfsnh78z7bjbfkdmj
+
+http://api.amp.active.com/camping/campsites/?apiKey=3vtcjh2jfsnh78z7bjbfkdmj&lengthOfStay=2&siteType=10001&parkId=120063&contractCode=CA&arvdate=11%2F28%2F13
--- searcher/views.py
+++ searcher/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
-
+import requests
+import cgi
 from searcher.models import CampgroundQuery
 
+# display the list of eligible queries
 def index(request):
-	latest_campgroundquery_list = CampgroundQuery.objects.order_by('-start_date')[:5]
+	latest_campgroundquery_list = CampgroundQuery.objects.filter(active=True).order_by('-start_date')[:5]
 	template = loader.get_template('searcher/index.html')
 	context = RequestContext(request, {
 		'latest_campgroundquery_list': latest_campgroundquery_list,
@@ -23,4 +26,14 @@ def results(request, campgroundquery_id):
 	return HttpResponse("You are looking at the results of CampgroundQuery %s" % campgroundquery_id)
 
 def execute(request, campgroundquery_id):
-	return HttpResponse("You have executed CampgroundQuery %s" % campgroundquery_id)
+	c = get_object_or_404(CampgroundQuery, pk=campgroundquery_id)
+	params = {
+		'parkId': c.campground.park_id,
+		'arvdate': c.start_date.strftime("%x"),
+		'lengthOfStay': c.stay_length,
+		'contractCode': 'CA',
+		'siteType': 10001,
+		'api_key': '3vtcjh2jfsnh78z7bjbfkdmj'}
+	req = requests.get("http://api.amp.active.com/camping/campsites/", params=params)
+	return HttpResponse("You have executed CampgroundQuery %s with URL %s<br><pre>%s</pre>" % 
+		(campgroundquery_id, req.url, cgi.escape(req.text)))
